83e851046b6ea5a17ffedba1a9cf554d160749d6
Moriah <moriahkwashington@gmail.com>
2017-10-04 22:03:34 -0400

    This is the first commit.

---
+++ Exercise10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your height in inches: ");
+
+            double height = Difference();
+            
+            if (height < 54)
+            {
+                Console.WriteLine("I'm sorry, you aren't tall enough to ride the Raptor. You need " + height + " more inches!" );
+            }
+
+        }
+
+
+
+        static double Difference()
+        {
+            double height = double.Parse(Console.ReadLine());
+            double heightDifference = 54 - height;
+
+            return heightDifference;
+        }
+            
+
+
+
+
+        
+    }
+}
